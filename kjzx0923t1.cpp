24d08dceca9d9f54c8353ac0861b117e808b7da4
JohnAppleseeder <1420780427@qq.com>
2017-09-27 21:28:15 +0800

    s

--- kjzx0923t1.cpp
+++ kjzx0923t1.cpp
@@ -16,3 +16,91 @@ int main(){
 http://blog.csdn.net/u010582475/article/details/47707739
 */
 
+
+/*
+#include<iostream>
+#define ll long long
+#define mod 10007;
+using namespace std;
+int damn(ll i,ll j){
+    if(i==j||j==1)return 1;
+    return (damn(i-1,j-1)+damn(i-1,j))%mod;
+}
+int main(){
+    int m,n;
+    cin>>m>>n;
+    cout<<damn(m,n);
+}*/
+
+#include<iostream>
+#include<algorithm>
+#define mod 10007;
+using namespace std;
+int main(){
+    int m,n;
+    int f[2000010]={1};
+    bool ok;
+    cin>>m>>n;
+    ok=n<(m/2);
+    if(ok==1){
+        for(int i=1;i<n;i++){
+            f[i]=1;
+            for(int j=i-1;j>=1;j--){
+              f[j]=(f[j]+f[j-1])%mod;
+             // cout<<f[j]<<' ';
+            }
+          //  cout<<"   "<<i<<'\n';
+        }
+        //cout<<'\n';
+        for(int i=n;i<=m-n;i++){
+            f[i]=1;
+            for(int j=n-1;j>=1;j--){
+                f[j]=(f[j]+f[j-1])%mod;
+               // cout<<f[j]<<' ';
+            }
+            //cout<<"   "<<i<<'\n';
+        }
+       // cout<<'\n';
+        for(int i=m-n+1;i<=m;i++){
+            f[i]=1;
+            for(int j=n-1;j>=i-m+n;j--){
+                f[j]=(f[j]+f[j-1])%mod;
+               // cout<<f[j]<<' ';
+            }
+         //   cout<<"   "<<i<<'\n';
+        }
+     //   cout<<'\n';
+        cout<<f[n-1];
+    }
+    if(ok==0){
+        for(int i=1;i<m-n;i++){
+            f[i]=1;
+            for(int j=i-1;j>=1;j--){
+              f[j]=(f[j]+f[j-1])%mod;
+              cout<<f[j]<<' ';
+            }
+            cout<<"   "<<i<<'\n';
+        }
+        cout<<'\n';
+        for(int i=m-n;i<=n;i++){
+            f[i]=1;
+            for(int j=i-1;j>i-m+n;j--){
+                f[j]=(f[j]+f[j-1])%mod;
+                cout<<f[j]<<' ';
+                
+            }
+            cout<<"   "<<i<<'\n';
+        }
+        cout<<'\n';
+        for(int i=n+1;i<m;i++){
+            f[i]=1;
+            for(int j=n-1;j>=i-m+n;j--){
+                f[j]=(f[j]+f[j-1])%mod;
+                cout<<f[j]<<' ';
+            }
+            cout<<"   "<<i<<'\n';
+        }
+        cout<<'\n';
+        cout<<f[n-1];
+    }
+}
