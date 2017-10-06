ab99a9313cbb6fcce03e2e49cbd06cc6702880c1
JohnAppleseeder <1420780427@qq.com>
2017-10-06 11:45:27 +0800

    asdf

--- luogu1120.cpp
+++ luogu1120.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
+#include<cstdio>
+#include<algorithm>
+bool cmp(int a,int b){
+    return a>b;
+}
 using namespace std;
 int main(){
+    int n;
+    scanf("%d",&n);
+    int a[70];
+    for(register int i=1;i<=n;i++){
+        scanf("%d",&a[i]);
+    }
+    sort(a,a+n+1,cmp);
+    int sum=0;
+    for(register int i=n;i>=1;i--)if(a[i]>50)n--;else break;
+    for(register int i=1;i<=n;i++)sum+=a[i];
+    for(register int i=1;i<=sum/2;i++)
+        if(sum%i==0)//get;
     
 }
--- noip16d2t1.cpp
+++ noip16d2t1.cpp
@@ -6,11 +6,8 @@ int main(){
     int a[2010][2010]={0};
     a[1][1]=1;
     
-    for(register int i=1;i<=2000;i++){
-        a[i][0]=1;//这里要0使整个式子平移
-        //或者不搞下面全改成2001
-        //所以不能naive一定要开大一点的
-        //卡死范围会翻车
+    for(register int i=1;i<=2001;i++){
+        a[i][1]=1;
         for(register int j=1;j<i;j++){
             a[i][j]=(a[i-1][j]+a[i-1][j-1])%k;
            // cout<<a[i][j]<<' ';
@@ -19,11 +16,13 @@ int main(){
       //  cout<<'\n';
     }
     
-    int pre[2010][2010]={0};
-    for(register int i=1;i<=2000;i++){
-        for(register int j=1;j<=2000;j++){//循环完整谢谢
-            pre[i][j]=pre[i-1][j]+pre[i][j-1]-pre[i-1][j-1];
-            if(a[i][j]==0&&j<=i)pre[i][j]++;//这里要加&&j<=i否则当n>m的时候就会gg
+    //int pre[2010][2010]={0};
+    for(register int i=1;i<=2001;i++){
+        for(register int j=1;j<=2001;j++){//循环完整谢谢
+            if(a[i][j]==0&&j<=i)a[i][j]++;//这里要加&&j<=i否则当n>m的时候就会gg
+            else a[i][j]=0;
+            a[i][j]=a[i-1][j]+a[i][j-1]-a[i-1][j-1];
+            
            // cout<<pre[i][j]<<' ';
         }
       //  cout<<'\n';
@@ -32,7 +31,7 @@ int main(){
     for(register int i=1;i<=t;i++){
         int n,m;
         cin>>n>>m;
-        cout<<pre[n+1][m+1]<<'\n';
+        cout<<a[n+1][m+1]<<'\n';
         //我他妈智熄
     }
     return 0;
