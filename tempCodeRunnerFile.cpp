2771ca60ee158364e6df0a4cddee79f642580630
VoidXia <1420780427@qq.com>
2018-11-11 20:42:48 +0800

    ;

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,33 +1,31 @@
-#include<iostream>
-#include<iomanip>
+#include<bits/stdc++.h>
 using namespace std;
- 
-int main()
-{
-	double f = 3.1415926535;
-	cout << "Enter the huashi temperature:" << endl;
-	//-----------------方法一-------------------
-	/*	cout.precision(3);		//调用cout的precision()函数设置或返回当前要被显示的浮点变量的位数（即浮点数的数字个数）
-	cout << fixed;		//fixed输出小数点后面三位有效数字
-	cout << "The Celsius temperature is: ";
-	cout << f << endl;		//输出小数点前后共三位有效数字
-	cout << f << endl;
-	*/
-	//----------------方法二--------------------
-	//使用setprecision()操作符，包含在在iomanip头文件中
-	cout << f << endl;				//默认输出六位
-	cout << setprecision(3);		//单用setprecision是设置显示的有效数字位数。
-	cout << setprecision(0) << f << endl;
-	cout << setprecision(1) << f << endl;
-	cout << setprecision(2) << f << endl;
-	cout << setprecision(3) << f << endl;
-	cout << setprecision(4) << f << endl;
-	cout << "---------------------------------" << endl;
-	cout <<setiosflags(ios::fixed);	//配合setprecision就是设置小数精度(小数点后的有效位数)
-	//cout << fixed;				//作为cout的一个参数是setiosflags(ios::fixed)的简写形式，等效于上一行
-	cout << setprecision(0) << f << endl;
-	cout << setprecision(1) << f << endl;
-	cout << setprecision(2) << f << endl;
-	cout << setprecision(3) << f << endl;
-	cout << setprecision(4) << f << endl;
+#define N 30010
+struct node{int u,v,n;}a[N];
+bool operator <(const node &a,const node &b){
+    return a.v<b.v;
+}
+int n,m;
+bool vis[N]={0};
+int main(){
+    int cnt=0;
+    cin>>n>>m;
+    for(int i=1;i<=m;i++)cin>>a[i].u>>a[i].v>>a[i].n;
+    sort(a+1,a+m+1);
+    for(int i=1;i<=m;i++){
+        int ans=0;
+        for(int j=a[i].u;j<=a[i].v;j++)if(vis[j]==1)ans++;
+        ans=a[i].n-ans;
+        //cout<<i<<' '<<ans<<'\n';
+        for(int j=a[i].v;j>=a[i].u&&ans>0;j--){
+            if(vis[j]==0){
+                vis[j]=1;
+                cnt++;
+                //cout<<j<<"vis\n";
+                ans--;
+            }
+            //if(ans==0)break;
+        }
+    }
+    cout<<cnt<<'\n';
 }
